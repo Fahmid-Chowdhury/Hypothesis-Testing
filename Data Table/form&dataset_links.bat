f5bebc36b223752736fb8ba293a4203fbc417e69
Fahmid Chowdhury <fahmidoct26@gmail.com>
2025-08-04 05:34:49 +0600

    final

---
+++ Data Table/form&dataset_links.bat
@@ -0,0 +1,5 @@
+@echo off
+
+REM URLs to open
+start "" "https://docs.google.com/forms/d/1oPgcP8RpYr8yKomlKoyKtbG02vGTcNlwjx99uNoWP98/edit"
+start "" "https://docs.google.com/spreadsheets/d/15WF0VjrBI0ZSn2WUYnBQNUlKklp98MOKKOo14nMBFJw/edit?resourcekey=&gid=459114186#gid=459114186"
